a994b4f81b38b8e4e4c9d4adfae7ccfd52ef166a
arifdzainal <mohdarif.dzainalabidin@gmail.com>
2023-10-19 21:14:56 +0800

    update redirect page

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 include 'class/db.php';
+header('Location: main.php');
+exit;
 ?>
 
 <!DOCTYPE html>
 <html lang="en" class="no-js">
     <head>
+        
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
