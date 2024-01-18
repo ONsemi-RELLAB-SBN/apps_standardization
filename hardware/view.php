5f792f716a7a891cd3c997a4f5b6cb34bd45e6e3
arifdzainal <mohdarif.dzainalabidin@gmail.com>
2024-01-18 10:56:38 +0800

    adjust the button space and make it readonly on view page

--- hardware/view.php
+++ hardware/view.php
@@ -113,17 +113,17 @@ $id = $_GET['view'];
                 <div class="tab-content" id="tabCapability">
                     <div class="row">
                         <div class="two columns"><label for="volt_rating">Voltage Rating</label></div>
-                        <div class="two columns"><input type="number" step="0.001" id="volt_rating" name="volt_rating" value="<?php echo $rowForm['voltage_rating']; ?>" required> </div>
+                        <div class="two columns"><input type="number" step="0.001" id="volt_rating" name="volt_rating" value="<?php echo $rowForm['voltage_rating']; ?>" required readonly> </div>
                         <div class="one columns"><label for="volt_rating" style="text-align: left"><b>V</b></label></div>
                         <div class="one columns">&nbsp;</div>
                         <div class="two columns"><label for="curr_rating">Current Rating</label></div>
-                        <div class="two columns"><input type="number" step="0.001" id="curr_rating" name="curr_rating" value="<?php echo $rowForm['current_rating']; ?>" required> </div>
+                        <div class="two columns"><input type="number" step="0.001" id="curr_rating" name="curr_rating" value="<?php echo $rowForm['current_rating']; ?>" required readonly> </div>
                         <div class="one columns"><label for="curr_rating" style="text-align: left"><b>A</b></label></div>
                         <div class="one columns">&nbsp;</div>
                     </div>
                     <div class="row">
                         <div class="two columns"><label for="temp_rating">Temp Rating</label></div>
-                        <div class="two columns"><input type="number" step="0.001" id="temp_rating" name="temp_rating" value="<?php echo $rowForm['temp_rating']; ?>" required> </div>
+                        <div class="two columns"><input type="number" step="0.001" id="temp_rating" name="temp_rating" value="<?php echo $rowForm['temp_rating']; ?>" required readonly> </div>
                         <div class="one columns"><label for="temp_rating" style="text-align: left"><b>&#8451;</b></label></div>
                         <div class="one columns">&nbsp;</div>
                         <div class="two columns"><label for="support_stress">Supported Stresses</label></div>
@@ -151,7 +151,7 @@ $id = $_GET['view'];
                                     <div class="three columns"><input type="text" id="pcb_material" name="pcb_material" value="<?php echo getParameterValue($rowForm['pcb_material']); ?>" required readonly /></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns"><label for="mb_dimension_l">Motherboard Dimension (L)</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="mb_dimension_l" name="mb_dimension_l" value="<?php echo $rowForm['mb_dimension_l']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="mb_dimension_l" name="mb_dimension_l" value="<?php echo $rowForm['mb_dimension_l']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="mb_dimension_l" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -160,7 +160,7 @@ $id = $_GET['view'];
                                     <div class="three columns"><input type="text" id="frame_material" name="frame_material" value="<?php echo getParameterValue($rowForm['frame_material']); ?>" required readonly /></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns"><label for="mb_dimension_w">(W)</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="mb_dimension_w" name="mb_dimension_w" value="<?php echo $rowForm['mb_dimension_w']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="mb_dimension_w" name="mb_dimension_w" value="<?php echo $rowForm['mb_dimension_w']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="mb_dimension_w" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -169,13 +169,13 @@ $id = $_GET['view'];
                                     <div class="three columns"><input type="text" id="board_coat" name="board_coat" value="<?php echo getParameterValue($rowForm['board_coating']); ?>" required readonly /></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns"><label for="mb_dimension_t">(T)</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="mb_dimension_t" name="mb_dimension_t" value="<?php echo $rowForm['mb_dimension_t']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="mb_dimension_t" name="mb_dimension_t" value="<?php echo $rowForm['mb_dimension_t']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="mb_dimension_t" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
                                 <div class="row">
                                     <div class="two columns"><label for="no_layers">Number of Layers</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="no_layers" name="no_layers" value="<?php echo $rowForm['no_layer']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="no_layers" name="no_layers" value="<?php echo $rowForm['no_layer']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="no_layers" style="text-align: left"><b>layers</b></label></div>
                                 </div>
                             </div>
@@ -212,17 +212,17 @@ $id = $_GET['view'];
                                 </div>
                                 <div class="row">
                                     <div class="two columns"><label for="socket_conn_qty">Socket/connector qty</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="socket_conn_qty" name="socket_conn_qty" value="<?php echo $rowForm['mb_socket_qty']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="socket_conn_qty" name="socket_conn_qty" value="<?php echo $rowForm['mb_socket_qty']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="socket_conn_qty" style="text-align: left"><b>pcs</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns"><label for="socket_conn_pin_qty">Socket/connector pin qty</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="socket_conn_pin_qty" name="socket_conn_pin_qty" value="<?php echo $rowForm['mb_socket_pin_qty']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="socket_conn_pin_qty" name="socket_conn_pin_qty" value="<?php echo $rowForm['mb_socket_pin_qty']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="socket_conn_pin_qty" style="text-align: left"><b>pins</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
                                 <div class="row">
                                     <div class="two columns"><label for="socket_con_pin_pitch">Socket/connector pin pitch</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="socket_con_pin_pitch" name="socket_con_pin_pitch" value="<?php echo $rowForm['mb_socket_pin_pitch']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="socket_con_pin_pitch" name="socket_con_pin_pitch" value="<?php echo $rowForm['mb_socket_pin_pitch']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="socket_con_pin_pitch" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns">
@@ -247,7 +247,7 @@ $id = $_GET['view'];
                             <div class="content">
                                 <div class="row">
                                     <div class="two columns"><label for="max_load_card_qty">Max load card qty</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="max_load_card_qty" name="max_load_card_qty" value="<?php echo $rowForm['lc_max_qty']; ?>" required></div>
+                                    <div class="two columns"><input type="number" step="0.001" id="max_load_card_qty" name="max_load_card_qty" value="<?php echo $rowForm['lc_max_qty']; ?>" required readonly></div>
                                     <div class="one columns"><label for="max_load_card_qty" style="text-align: left"><b>pcs</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns">
@@ -259,7 +259,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/004.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="load_card_pin_qty" name="load_card_pin_qty" value="<?php echo $rowForm['lc_pin_qty']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="load_card_pin_qty" name="load_card_pin_qty" value="<?php echo $rowForm['lc_pin_qty']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="load_card_pin_qty" style="text-align: left"><b>pins</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -273,7 +273,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/005.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="load_card_pin_pitch" name="load_card_pin_pitch" value="<?php echo $rowForm['lc_pin_pitch']; ?>" required></div>
+                                    <div class="two columns"><input type="number" step="0.001" id="load_card_pin_pitch" name="load_card_pin_pitch" value="<?php echo $rowForm['lc_pin_pitch']; ?>" required readonly></div>
                                     <div class="one columns"><label for="load_card_pin_pitch" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -288,7 +288,7 @@ $id = $_GET['view'];
                             <div class="content">
                                 <div class="row">
                                     <div class="two columns"><label for="max_prog_card_qty">Max program card qty</label></div>
-                                    <div class="two columns"><input type="number" step="0.001" id="max_prog_card_qty" name="max_prog_card_qty" value="<?php echo $rowForm['pc_max_qty']; ?>" required></div>
+                                    <div class="two columns"><input type="number" step="0.001" id="max_prog_card_qty" name="max_prog_card_qty" value="<?php echo $rowForm['pc_max_qty']; ?>" required readonly></div>
                                     <div class="one columns"><label for="max_prog_card_qty" style="text-align: left"><b>pcs</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns">
@@ -300,7 +300,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/006.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="prog_card_pin_qty" name="prog_card_pin_qty" value="<?php echo $rowForm['pc_pin_qty']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="prog_card_pin_qty" name="prog_card_pin_qty" value="<?php echo $rowForm['pc_pin_qty']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="prog_card_pin_qty" style="text-align: left"><b>pins</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -314,7 +314,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/007.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="prog_card_pin_pitch" name="prog_card_pin_pitch" value="<?php echo $rowForm['pc_pin_pitch']; ?>" required></div>
+                                    <div class="two columns"><input type="number" step="0.001" id="prog_card_pin_pitch" name="prog_card_pin_pitch" value="<?php echo $rowForm['pc_pin_pitch']; ?>" required readonly></div>
                                     <div class="one columns"><label for="prog_card_pin_pitch" style="text-align: left"><b>mm</b></label></div>
                                 </div>
                             </div>
@@ -335,7 +335,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/008.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="three columns"><input type="text" id="conn_type" name="conn_type" value="<?php echo getParameterValue($rowForm['connector_type']); ?>" required> </div>
+                                    <div class="three columns"><input type="text" id="conn_type" name="conn_type" value="<?php echo getParameterValue($rowForm['connector_type']); ?>" required readonly> </div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns">
                                         <label for="no_pins">Number of pins</label>
@@ -346,7 +346,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/009.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="no_pins" name="no_pins" value="<?php echo $rowForm['no_pin']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="no_pins" name="no_pins" value="<?php echo $rowForm['no_pin']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="no_pins" style="text-align: left"><b>pins</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -360,7 +360,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/009.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="pin_pitch" name="pin_pitch" value="<?php echo $rowForm['pin_pitch']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="pin_pitch" name="pin_pitch" value="<?php echo $rowForm['pin_pitch']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="pin_pitch" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                     <div class="two columns">
@@ -372,7 +372,7 @@ $id = $_GET['view'];
                                             <img id="myImg" src="../image/hardware/010.png" alt="image" style="width:100%" class="w3-modal-content w3-animate-zoom">
                                         </dialog>
                                     </div>
-                                    <div class="two columns"><input type="number" step="0.001" id="edge_thick" name="edge_thick" value="<?php echo $rowForm['edgefinger_thickness']; ?>" required> </div>
+                                    <div class="two columns"><input type="number" step="0.001" id="edge_thick" name="edge_thick" value="<?php echo $rowForm['edgefinger_thickness']; ?>" required readonly> </div>
                                     <div class="one columns"><label for="edge_thick" style="text-align: left"><b>mm</b></label></div>
                                     <div class="one columns">&nbsp;</div>
                                 </div>
@@ -384,7 +384,7 @@ $id = $_GET['view'];
                 <div class="tab-content" id="tabCapacity">
                     <div class="row">
                         <div class="two columns"><label for="max_dut_mb">Max DUT qty per motherboard</label></div>
-                        <div class="two columns"><input type="number" step="0.001" id="max_dut_mb" name="max_dut_mb" value="<?php echo $rowForm['max_dut_qty_mb']; ?>" > </div>
+                        <div class="two columns"><input type="number" step="0.001" id="max_dut_mb" name="max_dut_mb" value="<?php echo $rowForm['max_dut_qty_mb']; ?>" readonly> </div>
                         <div class="one columns"><label for="max_dut_mb" style="text-align: left"><b>DUTs</b></label></div>
                     </div>
                 </div>
