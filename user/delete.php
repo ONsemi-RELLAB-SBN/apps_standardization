c33d0de05f3a3df58d69a2cf3dfb31ea70c5e72c
arifdzainal <mohdarif.dzainalabidin@gmail.com>
2024-01-30 08:27:48 +0800

    update function to update / delete user access to system

--- user/delete.php
+++ user/delete.php
@@ -1,11 +1,56 @@
 <?php
-include "db_conn.php";
-$id = $_GET["id"];
-$sql = "DELETE FROM `sample` WHERE id = $id";
-$result = mysqli_query($conn, $sql);
 
-if ($result) {
-    header("Location: index.php?msg=Data deleted successfully");
-} else {
-    echo "Failed: " . mysqli_error($conn);
-}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Delete Record Confirmation</title>
+        <link rel="stylesheet" href="../css/skeleton.css"/>
+    </head>
+    <body>
+        <?php
+        // Check if a record ID is provided
+        include "db_conn.php";
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+
+            // Retrieve the record from the database
+            $query = "SELECT * FROM gest_user WHERE id = $id";
+            $result = mysqli_query($conn, $query);
+            $row = mysqli_fetch_assoc($result);
+
+            // Display the record details and confirmation form
+            if ($row) {
+                echo '<h4 style="border-left: none">Are you sure you want to delete this record?</h4>';
+                echo '<p>Name: ' . $row['name'] . '</p>';
+                echo '<p>Email: ' . $row['email'] . '</p>';
+                echo '<form method="post" action="delete.php?id='.$id.'">';
+                echo '<input type="hidden" name="id" value="' . $id . '">';
+                echo '<input type="submit" name="confirm" value="Yes">';
+                echo '<input type="submit" name="cancel" value="No">';
+                echo '</form>';
+            } else {
+                echo 'Record not found.';
+            }
+        } else {
+            echo 'Invalid record ID.';
+        }
+
+        // Handle the delete operation
+        if (isset($_POST['confirm'])) {
+            $id = $_GET['id'];
+            $sql = "DELETE FROM `gest_user` WHERE id = $id";
+            echo 'apa le benda nya ko nk uat ni ::: ' . $sql;
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                header("Location: index.php?msg=Data deleted successfully");
+            } else {
+                echo "Failed: " . mysqli_error($conn);
+            }
+        } else if (isset($_POST['cancel'])) {
+            header("Location: index.php?msg=Operation Cancelled");
+        }
+        ?>
+    </body>
+</html>
--- user/index.php
+++ user/index.php
@@ -11,6 +11,7 @@ include "db_conn.php";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <title>GEST USER</title>
     </head>
 
@@ -26,8 +27,9 @@ include "db_conn.php";
                 ' . $msg . '
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>';
-            } ?>
-            <a href="add-new.php" class="btn btn-dark mb-3">Add New</a>
+            }
+            ?>
+            <a href="add-new.php" class="btn btn-dark mb-3"><i class='bx bxs-user-plus bx-fw'></i>Add New</a>
 
             <table class="table table-hover text-center">
                 <thead class="table-dark">
@@ -69,5 +71,6 @@ include "db_conn.php";
         </div>
         <!-- Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     </body>
 </html>
